896c83ed23a26303a2e4af9ff0b1d7a3f6eedf1d
zalmai-zazai <shahzalmai98@gmail.com>
2024-12-06 16:04:38 -0800

    nextrequest added

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,9 +2,9 @@ import connectDB from "@/lib/db";
 import User from "@/models/User";
 import next from "next";
 import bcrypt from "bcrypt";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     await connectDB();
     const { username, email, password } = await req.json();
